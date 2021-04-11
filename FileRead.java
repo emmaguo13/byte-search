a41f9650a3600917fd58d8832f161cbf5a5c3ace
emmaguo13 <emmaguo@berkeley.edu>
2021-04-11 02:12:03 -0700

    working

--- FileRead.java
+++ FileRead.java
@@ -1,64 +1,113 @@
 import java.io.*;
 import java.util.*;
 
-class FileRead {
+class Node {
+    private int fileName;
+    private int position;
 
-    class Node {
-        public int value;
-        public int fileName;
-        public int position;
+    public Node (int fileName, int position) {
+        this.position = position;
+        this.fileName = fileName;
+    }
 
-        public Node (int value, int fileName, int position) {
-            this.value = value;
-            this.position = position;
-            this.fileName = fileName;
+    public int getFileName() {
+        return this.fileName;
+    }
+
+    public int getPos() {
+        return this.position;
+    } 
+
+
+}
+
+class FullMax {
+    private HashMap <ArrayList<Integer>, ArrayList<Node>> store;
+    private String[] files;
+
+    public FullMax(String[] files) {
+        this.store = new HashMap<ArrayList<Integer>, ArrayList<Node>>();
+        this.files = files;
+        for (int i = 0; i < files.length; i++ ) {
+            addToMap(i);
         }
     }
 
-    public static void main(String[] args) {
+    public HashMap <ArrayList<Integer>, ArrayList<Node>> completedHashMap() {
+        return this.store;
+    }
 
-            HashMap<Integer, ArrayList<Integer>> store = new HashMap<Integer, ArrayList<Integer>>();
-        //array: file name | file name | ... | position | position | ... 
-            String fname = args[0];
-            int fnum = Integer.parseInt(args[0].substring(args[0].length() - 1, args[0].length()));
+    private void addToMap(int num) {
+            //first file 
+            //String fname = args[0];
+            String fname = this.files[num];
+            int fnum = Integer.parseInt(fname.substring(fname.length() - 1, fname.length()));
             int position = 0;
             try {
-                FileInputStream fileReader = new FileInputStream(fname);
+                FileInputStream fileReader = new FileInputStream(fname);  
+                ArrayList<Integer> arrayBytes = new ArrayList<Integer>();
                 while (fileReader.available() > 0)
                 {
                     int curr = fileReader.read();
-                    System.out.println(curr);
-                    ArrayList<Integer> currPos = new ArrayList<Integer>();
-                    currPos.add(fnum);
-                    currPos.add(position);
-                    store.put(curr, currPos);
-                    position ++;
-                
+                    
+                    if (curr == 32) {
+                        position ++;
+                        System.out.println(arrayBytes.size() + " " + fnum);
+                        if (store.containsKey(arrayBytes)) {
+                            store.get(arrayBytes).add(new Node(fnum, position));
+                        } else {
+                            ArrayList<Node> currPos = new ArrayList<Node>();
+                            currPos.add(new Node (fnum, position));
+                            store.put(arrayBytes, currPos);
+                        }
+                        arrayBytes = new ArrayList<Integer>();
+
+                    } else {
+                        arrayBytes.add(curr);
+                    }
+                    
                 }
-                fileReader.close();
-            }  catch (FileNotFoundException ffe) {
-                System.err.println("Error: the file was not found!");
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            } 
-            // make pairings << hashmap for each pair (WAY MORE SPACE)
-                // as you make one pairing, store the largest value of the pairing
-                // as you make your next pairings, keep checking if they meet the previous pairing. 
-                //if one member does, add the one member to the largest value dictionary type thing
-                // if both do, add both members 
-                // discard if you have a number pairing that is bigger 
-                //the pairings (hashmap) will only have the full first array and the matching second array items
-            // cehck which hashmap has largest values
-            // then add on other 
-            //traverse through first and second array
-            // add each other array to the first and second array pairings 
-            // a b c >> ab bc ca >> abc 
-            // a b c d e >> ab ac ad ae bc bd be cd ce de abc, etc
+                    fileReader.close();
+                    
+                }  catch (FileNotFoundException ffe) {
+                    System.err.println("Error: the file was not found!");
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } 
+                
+    }
+}
 
-            //sort each array using max priority queue
-            //pull from each pq 
-            //nodes in pq 
+class FileRead {
 
+    public static void main(String[] args) {
+        //PriorityQueue<Integer> queue = new PriorityQueue<>(10, Collections.reverseOrder());
+        //HashMap<Integer, ArrayList<Node>> store = new HashMap<Integer, ArrayList<Node>>();
+        //array: file name | file name | ... | position | position | ... 
+        FullMax data = new FullMax(args);
+        HashMap <ArrayList<Integer>, ArrayList<Node>> cleanHashMap = data.completedHashMap();
+        int max = 0;
+        ArrayList<Integer> arrayResult = new ArrayList<Integer>();
+        for (ArrayList<Integer> key : cleanHashMap.keySet()) {
+            if (cleanHashMap.get(key).size() > 1) {
+                if (key.size() > max) {
+                    max = key.size();
+                    arrayResult = key;
+                } 
+            }
+        }
+
+        System.out.println("Maximum shared value: " + max);
+        ArrayList result = cleanHashMap.get(arrayResult);
+        System.out.println("In files:");
+        for (int i = 0; i < result.size(); i++ ) {
+            System.out.println(((Node)result.get(i)).getFileName()); 
+        }
+        System.out.println("Position:") ;
+        for (int i = 0; i < result.size(); i++ ) {
+            System.out.println(((Node)result.get(i)).getPos()); 
+        }
+    }
 
         
 
@@ -68,4 +117,3 @@ class FileRead {
     
 
 
-}
